f98e2b2115bb88baf234173a8aa9f427512dae6d
kyawmoehan <kyawmoehan012@gmail.com>
2020-06-18 18:33:10 +0630

    URL Shortener

---
+++ server/index.js
@@ -0,0 +1,91 @@
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const yup = require("yup");
+const monk = require("monk");
+const { nanoid } = require("nanoid");
+
+require("dotenv").config();
+
+const app = express();
+
+// mongodb connection
+const db = monk(process.env.MONGO_URI);
+const urls = db.get("urls");
+urls.createIndex({ slug: 1 }, { unique: true });
+
+// middleware
+app.use(helmet());
+app.use(morgan("tiny"));
+app.use(cors());
+app.use(express.json());
+app.use(express.static("./public"));
+
+app.get("/:id", async (req, res) => {
+  const { id: slug } = req.params;
+  console.log(slug);
+  try {
+    const url = await urls.findOne({ slug });
+    if (url) {
+      res.redirect(url.url);
+    }
+    res.redirect(`/?error=${slug} not found`);
+  } catch (error) {
+    res.redirect(`/?error=Link not found`);
+  }
+});
+
+const schema = yup.object().shape({
+  slug: yup
+    .string()
+    .trim()
+    // eslint-disable-next-line no-useless-escape
+    .matches(/[\w\-]/i),
+  url: yup.string().trim().url().required(),
+});
+
+app.post("/url", async (req, res, next) => {
+  let { slug, url } = req.body;
+  try {
+    await schema.validate({
+      slug,
+      url,
+    });
+    if (!slug) {
+      slug = nanoid(5);
+    } else {
+      const existing = await urls.findOne({ slug });
+      if (existing) {
+        throw new Error("Slug in use!");
+      }
+    }
+    slug = slug.toLowerCase();
+    const newUrl = {
+      url,
+      slug,
+    };
+    const created = await urls.insert(newUrl);
+    res.json(created);
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.use((error, req, res) => {
+  if (error.status) {
+    res.status(error.status);
+  } else {
+    res.status(500);
+  }
+  res.json({
+    message: error.message,
+    stack: process.env.NODE_ENV === "production" ? "" : error.stack,
+  });
+});
+
+// eslint-disable-next-line no-undef
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Listening at http://localhost:${PORT}`);
+});
